7990ea0b20df9e98ad607f89d873f2c4c1a8d772
DocMonster7 <errol.joshua7@gmail.com>
2024-01-02 12:05:00 +0530

    initial Commit

---
+++ variants/ELi_McM_D_ESP32/pins_arduino.h
@@ -0,0 +1,102 @@
+#ifndef Pins_Arduino_h
+#define Pins_Arduino_h
+
+#include <stdint.h>
+
+static const uint8_t TIMEOUT = 1000;
+static const uint8_t ADC_MODE = 0;
+static const uint8_t DAC_MODE = 1;
+
+static const uint8_t C_A = 36;
+static const uint8_t C_B = 39;
+static const uint8_t C_C = 25;
+static const uint8_t C_D = 35;
+static const uint8_t C_E = 34;
+static const uint8_t C_F = 23;
+static const uint8_t C_G = -1;
+static const uint8_t C_H = -2;
+static const uint8_t C_I = 22;
+static const uint8_t C_J = 18;
+static const uint8_t C_K = 05;
+static const uint8_t C_L = 00;
+static const uint8_t C_M = 13;
+static const uint8_t C_N = 12;
+static const uint8_t C_O = 26;
+static const uint8_t C_P = 33;
+
+static const uint8_t C1 = 36;
+static const uint8_t C2 = 39;
+static const uint8_t C3 = 25;
+static const uint8_t C4 = 35;
+static const uint8_t C5 = 34;
+static const uint8_t C6 = 23;
+static const uint8_t C7 = -1;
+static const uint8_t C8 = -2;
+static const uint8_t C9 = 22;
+static const uint8_t C10 = 18;
+static const uint8_t C11 = 05;
+static const uint8_t C12 = 00;
+static const uint8_t C13 = 13;
+static const uint8_t C14 = 12;
+static const uint8_t C15 = 26;
+static const uint8_t C16 = 33;
+
+static const uint8_t I1 = 0;
+static const uint8_t I2 = 1;
+static const uint8_t I3 = 2;
+static const uint8_t I4 = 3;
+static const uint8_t I5 = 4;
+static const uint8_t I6 = 5;
+static const uint8_t I7 = 6;
+static const uint8_t I8 = 7;
+static const uint8_t I9 = 8;
+static const uint8_t I10 = 9;
+static const uint8_t I11 = 10;
+static const uint8_t I12 = 11;
+static const uint8_t I13 = 12;
+static const uint8_t I14 = 13;
+static const uint8_t I15 = 14;
+static const uint8_t I16 = 15;
+
+static const uint8_t O1 = 0;
+static const uint8_t O2 = 1;
+static const uint8_t O3 = 2;
+static const uint8_t O4 = 3;
+static const uint8_t O5 = 4;
+static const uint8_t O6 = 5;
+static const uint8_t O7 = 6;
+static const uint8_t O8 = 7;
+static const uint8_t O9 = 8;
+static const uint8_t O10 = 9;
+static const uint8_t O11 = 10;
+static const uint8_t O12 = 11;
+static const uint8_t O13 = 12;
+static const uint8_t O14 = 13;
+static const uint8_t O15 = 14;
+static const uint8_t O16 = 15;
+
+static const uint8_t MISO = 15;
+static const uint8_t MOSI = 2;
+static const uint8_t SCLK = 4;
+
+static const uint8_t SCL = 27;
+static const uint8_t SDA = 14;
+
+static const uint8_t AMOSI = 19;
+static const uint8_t AMISO = 21;
+
+static const uint8_t ANALOG_IO = 32;
+static const uint8_t R_LED = 0;
+static const uint8_t G_LED = 5;
+static const uint8_t B_LED = 18;
+
+#ifndef BOARDCONF  
+	struct boardconf{
+	  uint8_t reserved;
+	  uint8_t brdtype;
+	  uint8_t brdno;
+	};
+	#define BOARDCONF
+#endif
+
+#endif /* Pins_Arduino_h */
