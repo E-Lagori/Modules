010b5881d8a5f8f08cbd54ca4359e33d7506ba0a
DocMonster7 <errol.joshua7@gmail.com>
2024-01-03 16:00:09 +0530

    chnaged to previous version

--- D.TEJ.32/1.0.0/variants/D.TEJ.32/variant.h
+++ D.TEJ.32/1.0.0/variants/D.TEJ.32/variant.h
@@ -27,18 +27,18 @@ extern UARTClass uart;
 //  * SPI Interfaces
 //  */
 
-// #define SPI_INTERFACES_COUNT 4
-// #define IIC_INTERFACES_COUNT 2
-// #define UART_INTERFACES_COUNT 3
+#define SPI_INTERFACES_COUNT 4
+#define IIC_INTERFACES_COUNT 2
+#define UART_INTERFACES_COUNT 3
 
-// #define SPI_REG(id, offset) ((id==0)?SPI0_REG(offset):(id==1)?SPI1_REG(offset):(id==2)?SPI2_REG(offset):SPI3_REG(offset))
+#define SPI_REG(id, offset) ((id==0)?SPI0_REG(offset):(id==1)?SPI1_REG(offset):(id==2)?SPI2_REG(offset):SPI3_REG(offset))
 
-// #define IIC_REG(id, offset) ((id==0)?IIC0_REG(offset):(id==1)?IIC1_REG(offset):(id==8)?IIC8_REG(offset):IIC0_REG(offset))
+#define IIC_REG(id, offset) ((id==0)?IIC0_REG(offset):(id==1)?IIC1_REG(offset):(id==8)?IIC8_REG(offset):IIC0_REG(offset))
 
-// #define UART_REG(id, offset) ((id==0)?UART0_REG(offset):(id==1)?UART1_REG(offset):(id==2)?UART2_REG(offset):UART0_REG(offset))
+#define UART_REG(id, offset) ((id==0)?UART0_REG(offset):(id==1)?UART1_REG(offset):(id==2)?UART2_REG(offset):UART0_REG(offset))
 
-// #define TIMER_REG(id, offset) ((id==0)?TIMER0_REG(offset):(id==1)?TIMER1_REG(offset):(id==2)?TIMER2_REG(offset):TIMER0_REG(offset))
+#define TIMER_REG(id, offset) ((id==0)?TIMER0_REG(offset):(id==1)?TIMER1_REG(offset):(id==2)?TIMER2_REG(offset):TIMER0_REG(offset))
 
-//#define IIC_REG(offset) IIC8_REG(offset)
-//#define IIC_REGP(i) _REG32P(IIC0_BASE_ADDR, (i))
+#define IIC_REG(offset) IIC8_REG(offset)
+#define IIC_REGP(i) _REG32P(IIC0_BASE_ADDR, (i))
 #endif
